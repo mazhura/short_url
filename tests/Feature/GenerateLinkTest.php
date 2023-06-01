3b53406c0351be6a5fdea1b1632336f3a0d4082f
Mykhailo Mazhura <mazhura1@gmail.com>
2023-06-02 01:31:32 +0300

    tests

--- tests/Feature/GenerateLinkTest.php
+++ tests/Feature/GenerateLinkTest.php
@@ -2,12 +2,15 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class GenerateLinkTest extends TestCase
 {
+    use DatabaseTransactions;
+
     public function test_link_validator_fail()
     {
         $response = $this->put('/', ['inputUrl' => '']);
@@ -22,5 +25,19 @@ class GenerateLinkTest extends TestCase
         $response = $this->put('/', ['inputUrl' => 'http://google.com']);
 
         $response->assertStatus(200);
+    }
+
+    public function test_link_redirect()
+    {
+        $url = 'http://google.com';
+
+        $response = $this->put('/', ['inputUrl' => $url]);
+        $response->assertStatus(200);
+
+        $outputUrl = $response->json('outputUrl');
+
+        $redirectRequest = $this->get($outputUrl);
+        $redirectRequest->assertStatus(302);
+        $redirectRequest->assertRedirect($url);
     }
 }
